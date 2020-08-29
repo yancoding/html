67c8345254bd5d628ad52739e5fb70320c412326
yan <yancoding@qq.com>
2020-08-29 23:13:15 +0800

    调整

--- src/components/disk/index.vue
+++ src/components/disk/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
+  <div v-loading="loading">
+    <el-breadcrumb separator="/">
+        <el-breadcrumb-item v-for="item of currentPath.split('/')" :key="item">{{item}}</el-breadcrumb-item>
+    </el-breadcrumb>
     <template v-for="item of pathContent">
       <div :key="item.path" style="display: inline-block;">
         <el-button type="text"  v-if="item.type === 'dir'" @click="getPathContent(item.path)">{{item.path}}</el-button>
         <div v-else-if="item.type === 'file'" style="display: inline-block;">
-          <el-image v-if="item.mime.indexOf('image') !== -1" :src="item.url" fit="contain" style="width: 200px; height: 200px;"></el-image>
+          <el-image
+            v-if="item.mime.indexOf('image') !== -1"
+            :src="item.url" fit="contain"
+            :preview-src-list="[item.url]"
+            style="width: 200px; height: 200px;"></el-image>
           <audio v-else-if="item.mime.indexOf('audio') !== -1" :src="item.url" controls></audio>
           <video v-else-if="item.mime.indexOf('video') !== -1" :src="item.url" controls></video>
         </div>
@@ -18,20 +25,24 @@ export default {
   data() {
     return {
       pathContent: [],
+      currentPath: '.',
+      loading: false,
     }
   },
   methods: {
     getPathContent(dir) {
+      this.loading = true
       this.$http.post('http://localhost:8081/disk/dir', {
         dir,
       })
         .then(res => {
-          console.log(res)
           if (res.data.success) {
             this.pathContent = res.data.data
+            this.currentPath = dir
           }
         })
         .catch(err => console.log(err))
+        .finally(() => this.loading = false)
     },
   },
   mounted() {
